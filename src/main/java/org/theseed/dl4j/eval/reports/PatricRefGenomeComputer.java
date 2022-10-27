bcda34f2137fbd94e5df319d4b7af51efb19fd19
Bruce Parrello <bparrello@figresearch.com>
2022-10-27 11:38:05 -0500

    Added support for a cached reference-genome database.

--- src/main/java/org/theseed/dl4j/eval/reports/PatricRefGenomeComputer.java
+++ src/main/java/org/theseed/dl4j/eval/reports/PatricRefGenomeComputer.java
@@ -4,12 +4,14 @@
 package org.theseed.dl4j.eval.reports;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.theseed.dl4j.eval.stats.GenomeStats;
 import org.theseed.genome.Genome;
+import org.theseed.genome.GenomeMultiDirectory;
 import org.theseed.p3api.P3Connection;
 import org.theseed.p3api.P3Genome;
 import org.theseed.proteins.kmers.KmerCollectionGroup;
@@ -33,12 +35,15 @@ public class PatricRefGenomeComputer extends RefGenomeComputer {
     private Map<String, Genome> referenceBuffer;
     /** connection to PATRIC */
     private P3Connection p3;
+    /** master directory for reference genomes */
+    private GenomeMultiDirectory fileCache;
 
     /**
      * Initialize the reference genome computer from the files in a specified
      * evaluation directory.
      *
      * @param modelDir	source evaluation directory
+     *
      * @throws IOException
      */
     public PatricRefGenomeComputer(File modelDir) throws IOException {
@@ -47,8 +52,30 @@ public class PatricRefGenomeComputer extends RefGenomeComputer {
         this.p3 = new P3Connection();
         // Create the reference buffer.
         this.referenceBuffer = new HashMap<String, Genome>();
-        // Read in the reference-genome database.
-        File refGenomeFile = new File(modelDir, "refGenomes.fa");
+        // Read in the reference-genome database.  First, we hope to find a full RepDb.
+        File refGenomeDir = new File(modelDir, "RefDb");
+        if (refGenomeDir.isDirectory()) {
+            log.info("Reference genome cache available.");
+            // Read the seed protein FASTA.
+            this.readRefFasta(new File(refGenomeDir, "refGenomes.fa"));
+            // Load the master directory containing the downloaded genomes.
+            this.fileCache = new GenomeMultiDirectory(refGenomeDir);
+        } else {
+            log.warn("No cache available. Reference genomes will be downloaded as needed.");
+            this.readRefFasta(new File(modelDir, "refGenomes.fa"));
+            // Denote we have no file cache.
+            this.fileCache = null;
+        }
+    }
+
+    /**
+     * Read in the reference genome seed proteins.
+     *
+     * @param refGenomeFile		file containing the reference genome IDs, names, and seed protein sequences
+     *
+     * @throws FileNotFoundException
+     */
+    protected void readRefFasta(File refGenomeFile) throws FileNotFoundException {
         try (FastaInputStream refStream = new FastaInputStream(refGenomeFile)) {
             // Create the kmer database object.
             this.referenceGenomes = new KmerCollectionGroup();
@@ -58,7 +85,6 @@ public class PatricRefGenomeComputer extends RefGenomeComputer {
             }
             log.info("{} reference genomes read.", this.referenceGenomes.size());
         }
-
     }
 
     @Override
@@ -86,21 +112,38 @@ public class PatricRefGenomeComputer extends RefGenomeComputer {
     public Genome computeRef(GenomeStats gReport) {
         Genome retVal = null;
         String seedProt = gReport.getSeed();
+        String genomeId = gReport.getId();
         // Find the closest genome in the reference genome database.
         log.info("Computing reference genome for {}: {}", gReport.getId(), gReport.getName());
-        KmerCollectionGroup.Result refGenomeData = this.referenceGenomes.getBest(seedProt);
+        KmerCollectionGroup.Result refGenomeData = this.referenceGenomes.getBestComparator(seedProt, genomeId);
         double refGenomeDistance = refGenomeData.getDistance();
         String refGenomeId = refGenomeData.getGroup();
         if (refGenomeId == null) {
             log.info("No reference genome found for {}.", gReport.getId());
         } else if (refGenomeDistance > MAX_GENOME_DIST) {
             log.info("Reference genome {} found, but distance of {} exceeds the maximum of {}.", refGenomeId, refGenomeDistance, MAX_GENOME_DIST);
-        } else if (refGenomeId.contentEquals(gReport.getId())) {
-            log.info("Genome {} is a reference to itself.  No useful data is available.", refGenomeId);
         } else {
             // Read in the genome and buffer it in case we reuse it.
-            retVal = this.referenceBuffer.computeIfAbsent(refGenomeId, k -> P3Genome.load(p3, k, P3Genome.Details.PROTEINS));
+            retVal = this.referenceBuffer.computeIfAbsent(refGenomeId, k -> getGenome(k));
         }
+        return retVal;
+    }
+
+    /**
+     * Retrieve the genome with the specified ID for use as a reference genome.
+     *
+     * @param genomeId	ID of the desired reference genome
+     *
+     * @return the genome with the specified ID
+     */
+    protected Genome getGenome(String genomeId) {
+        Genome retVal = null;
+        if (this.fileCache != null)
+            retVal = this.fileCache.get(genomeId);
+        // Here the genome is missing, which is an error, or we don't have a file cache.  Either
+        // way, we can recover by downloading.
+        if (retVal == null)
+            retVal = P3Genome.load(p3, genomeId, P3Genome.Details.PROTEINS);
         return retVal;
     }
 }
---
+++ src/test/java/org/theseed/dl4j/eval/reports/TestRefComputer.java
@@ -0,0 +1,83 @@
+/**
+ *
+ */
+package org.theseed.dl4j.eval.reports;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.theseed.dl4j.eval.stats.GenomeStats;
+import org.theseed.genome.Genome;
+import org.theseed.genome.iterator.GenomeSource;
+import org.theseed.utils.ParseFailureException;
+
+/**
+ * @author Bruce Parrello
+ *
+ */
+class TestRefComputer {
+
+    @Test
+    void testP3RefComputer() throws IOException, ParseFailureException {
+        // Test a ref computer with a hash.
+        RefGenomeComputer computer = new PatricRefGenomeComputer(new File("data", "Eval.Fake"));
+        // We need to read in the test genomes and build the genome reports.
+        GenomeSource testGenomes = GenomeSource.Type.DIR.create(new File("data", "genomes"));
+        GenomeStats[] reports = new GenomeStats[testGenomes.size()];
+        int rIdx = 0;
+        for (Genome genome : testGenomes) {
+            final var report = new GenomeStats(genome);
+            // We need to fill in the seed protein.
+            genome.getPegs().stream().filter(x ->
+                    x.getPegFunction().contentEquals("Phenylalanyl-tRNA synthetase alpha chain (EC 6.1.1.20)"))
+                    .forEach(x -> report.countSeed(x.getProteinTranslation()));
+            reports[rIdx] = report;
+            rIdx++;
+        }
+        // Set up the reference computer.
+        computer.setupReferences(reports);
+        testReferences(computer, reports);
+        // Now try again without a cache.
+        computer = new PatricRefGenomeComputer(new File("data", "Eval.P3"));
+        computer.setupReferences(reports);
+        testReferences(computer, reports);
+    }
+
+    /**
+     * Test the reference genomes for the specified reference computer.
+     *
+     * @param computer	reference computer
+     * @param reports	GenomeStats array
+     */
+    protected void testReferences(RefGenomeComputer computer, GenomeStats[] reports) {
+        // Test the reference genomes.
+        assertThat(reports[0].getId(), equalTo("1001240.4"));
+        Genome genome = computer.ref(reports[0].getGenome());
+        assertThat(genome.getId(), equalTo("1001240.6"));
+        assertThat(genome.getName(), equalTo("Cryobacterium roopkundense strain DSM 21065"));
+        assertThat(reports[0].getId(), equalTo("1001240.4"));
+        assertThat(reports[1].getId(), equalTo("1001240.6"));
+        genome = computer.ref(reports[1].getGenome());
+        assertThat(genome.getId(), equalTo("1001240.4"));
+        assertThat(genome.getName(), equalTo("Cryobacterium roopkundense RuG17"));
+        assertThat(reports[2].getId(), equalTo("1123349.3"));
+        genome = computer.ref(reports[2].getGenome());
+        assertThat(genome.getId(), equalTo("1123350.4"));
+        assertThat(genome.getName(), equalTo("Tepidibacter thalassicus DSM 15285"));
+        assertThat(reports[3].getId(), equalTo("1144273.3"));
+        genome = computer.ref(reports[3].getGenome());
+        assertThat(genome, nullValue());
+        assertThat(reports[4].getId(), equalTo("29391.22"));
+        genome = computer.ref(reports[4].getGenome());
+        assertThat(genome.getId(), equalTo("562982.3"));
+        assertThat(genome.getName(), equalTo("Gemella morbillorum M424"));
+        assertThat(reports[5].getId(), equalTo("511145.12"));
+        genome = computer.ref(reports[5].getGenome());
+        assertThat(genome, nullValue());
+    }
+
+}
